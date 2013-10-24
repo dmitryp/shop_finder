107bc57521c62dd611f4feeb27987e8647381522
Dmitry Penkin <dr.demax@gmail.com>
2013-10-24 20:44:25 +0700

    Fix for products having video thumbnail

--- lib/shop_finder/shops/wildberries.rb
+++ lib/shop_finder/shops/wildberries.rb
@@ -7,9 +7,9 @@ module ShopFinder
 
       def self.parse_images(page)
         uri = page.uri
-        page.search('ul.carousel/li/a').map do |a|
-          uri.scheme + ':' + a.attributes['rev'].value
-        end
+        page.search('ul.carousel/li/a.enabledZoom').map do |a|
+          uri.scheme + ':' + a.attributes['rev'].value unless a.attributes['rev'].nil?
+        end.compact
       end
     end
   end
